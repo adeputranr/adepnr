fb2ecc0f006c756e27472b21da1d5651da11751c
adeputranr <adeputranr3@gmail.com>
2025-07-20 21:02:55 +0700

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import asyncio
 from bot import Bot
 
-async def main():
-    bot = Bot()
-    await bot.start()
-    await asyncio.Event().wait()  # biar bot tetap hidup
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    Bot().run()
